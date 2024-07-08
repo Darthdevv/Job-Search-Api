463b5f8cd86b6367b733f51be38dd4fd56266b31
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-07-08 03:35:51 +0300

    💼 feat: getCompanyData and searcgCompanyDataByName functionality

--- controllers/company.controllers.js
+++ controllers/company.controllers.js
@@ -60,10 +60,41 @@ export const updateCompanyData = catchAsync(async (req, res, next) => { });
 export const deleteCompanyData = catchAsync(async (req, res, next) => { });
 
 
-export const getCompanyData = catchAsync(async (req, res, next) => { });
+export const getCompanyData = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Company.find(), req.query)
+    .sort()
+    .paginate();
+  const companies = await features.query;
+
+  if (!companies) {
+    return next(new appError("No Companies Found", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    requestedAt: req.requestTime,
+    results: companies.length,
+    data: { companies },
+  });
+});
 
 
-export const searchCompanyByBame = catchAsync(async (req, res, next) => { });
+export const searchCompanyByBame = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Company.find(), req.query)
+    .filter();
+  const companies = await features.query;
+
+  if (!companies) {
+    return next(new appError("No Companies Found", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    requestedAt: req.requestTime,
+    results: companies.length,
+    data: { companies },
+  });
+});
 
 
 export const GetApplicationsForSpecificJobs = catchAsync(async (req, res, next) => {});
--- routes/company.routes.js
+++ routes/company.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addCompanyData } from "../controllers/company.controllers.js";
+import { addCompanyData, getCompanyData, searchCompanyByBame } from "../controllers/company.controllers.js";
 import authenticationHandler from "../middlewares/auth/authentication.middleware.js";
 import authorizationHandler from "../middlewares/auth/authorization.middleware.js";
 import { roles } from "../utils/customData.js";
@@ -7,5 +7,7 @@ import { roles } from "../utils/customData.js";
 const router = Router();
 
 router.route('/').post(authenticationHandler, authorizationHandler(roles.HR_ONLY), addCompanyData)
+router.route('/').get(authenticationHandler, authorizationHandler(roles.HR_ONLY), getCompanyData)
+router.route('/search').get(authenticationHandler, authorizationHandler(roles.USER_HR), searchCompanyByBame)
 
 export default router;
