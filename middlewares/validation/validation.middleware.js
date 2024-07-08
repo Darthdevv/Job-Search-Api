c9a86d7fa4b7f60d7f5b58ea9830f42d4cfb0471
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-07-09 02:48:16 +0300

    💼 chore: adding joi and adding validation middleware

---
+++ middlewares/validation/validation.middleware.js
@@ -0,0 +1,24 @@
+import appError from '../../utils/appError.js'
+
+const reqKeys = ["body", "params", "query", "headers"];
+
+const validationHandler = (schema) => {
+  return (req, res, next) => {
+    let validationErrors = [];
+
+    for (const key of reqKeys) {
+
+      const validationResult = schema[key]?.validate(req[key], { abortEarly: false });
+
+      if (validationResult?.error) {
+        validationErrors.push(validationResult.error.details);
+      }
+    }
+
+    validationErrors.length
+      ? next(new appError(`Validation Error: ${validationErrors}`, 400))
+      : next();
+  }
+}
+
+export default validationHandler;
